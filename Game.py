f2b440ffbfb267672a3fc6a308aaccfac18faedf
GianMarcoZampa <gianmarco.zampa@gmail.com>
2019-10-22 09:52:35 +0200

    Add window resizing feature

--- Game.py
+++ Game.py
@@ -6,20 +6,26 @@ from Player import Player
 ws = 1000  # Screen's width
 hs = 750  # Screen's height
 bg = pygame.image.load('images\Background\BG.png')  # Background image
+fps = 60
 
 pygame.init()
-win = pygame.display.set_mode((ws, hs))
+win = pygame.display.set_mode((ws, hs), pygame.RESIZABLE)
 pygame.display.set_caption('Platform Game')
 
-player_1 = Player(0, 600)
+player_1 = Player(0,0)
+player_1.y = ws - player_1.height*3.75
 
 run = True
 while run:
-    pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(fps)
 
     for event in pygame.event.get():
         if event.type is pygame.QUIT:
             run = False
+        if event.type is pygame.VIDEORESIZE:
+            ws, hs = event.w, event.h
+            win = pygame.display.set_mode((ws, hs), pygame.RESIZABLE)
+            bg = pygame.transform.scale(bg, (ws, hs))
 
     win.blit(bg, (0, 0))
 
